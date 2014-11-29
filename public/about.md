5a2d2083bf6a6dc300fc1721395a8edff7ff441a
Welling Guzman <hola@wellingguzman.com>
2014-11-28 23:34:57 -0400

    going live

---
+++ public/about.md
@@ -0,0 +1,12 @@
+Howdy, Partner, My name is Welling Guzman as you have probably know, I am a Software & Web Engineer from the Dominican Republic, passionate about building software, make it better and reinventing the wheel.
+
+![](/images/me.jpg)
+
+This is the place where I am going to publish anything I find interesting, problem I had and solved, experiments with technologies, programming languages, tools or anything else. Things I have learned and want to share.
+
+This is also a way to practice and really do my writing.
+
+- You can reach me at hola AT this domain
+- You can tweet me at [@WellingGuzman](https://twitter.com/WellingGuzman)
+- You can fork me at or I can fork you from [WellingGuzman](https://github.com/WellingGuzman)
+- You can send me money to [my bank account](http://www.stopfraud.gov/report.html)
---
+++ server.js
@@ -0,0 +1,108 @@
+var http = require('http');
+var router = require('router-stupid');
+var harp = require ('harp');
+var moment = require('moment');
+var Highlights = require('highlights');
+var cheerio = require('cheerio');
+var S = require('string');
+var glob = require('glob');
+var st = require('st');
+
+var mount;
+var route = router();
+var outputPath = __dirname + '/www';
+var port = process.env.PORT || 9000;
+var pkg = require('./package');
+
+global.moment = moment;
+global.highlighter = new Highlights();
+global.cheerio = cheerio;
+global.S = S;
+global.version = pkg.version.split('.').slice(0, 2).join('.');
+
+var server = function (root) {
+  // manually glob all the .html files so that we can navigate
+  // without .html on the end of the urls
+  glob('**/*.html', {
+    cwd: root,
+    dot: false
+  }, function (er, files) {
+    htmlFiles = files.map(function (file) {
+      return '/' + file;
+    });
+  });
+
+  // use st module for static cached routing
+  mount = st({
+    path: root,
+    url: '/',
+    index: 'index.html', // server index.html for directories
+    passthrough: true // pass through if not found, so we can send 404
+  });
+  console.log('compilation complete');
+};
+
+function run() {
+  if (process.env.NODE_ENV === 'production') {
+    route.get('*', function (req, res, next) {
+
+      // simplify the url (remove the ?search) and test if
+      // we have a file that exists (in `htmlFiles`)
+      req.url = req.url.replace(/\?.*$/, '').replace(/(.)\/$/, '$1');
+      if (htmlFiles.indexOf(req.url + '.html') !== -1) {
+        // then we requested /foo/bar and we know there's a
+        // generated file that matches
+        req.url += '.html';
+      }
+
+      // if our server is ready, respond using the st module
+      // and if it's a 404, respond with `serve404`.
+      if (mount) {
+        mount(req, res, function serve404() {
+          res.writeHead(404);
+          res.end(fourohfour);
+        });
+      } else {
+        res.writeHead(404);
+        res.end();
+      }
+    });
+
+    console.log('Running harp-static on ' + port);
+    http.createServer(route).listen(port);
+
+    fourohfour = require('fs').readFileSync(outputPath + '/404.html');
+    server(outputPath, port);
+  } else {
+    route.all('*', function (req, res, next){
+      //@todo fix this
+      if (req.url == '/writing') {
+        req.url = '/writing/';
+      }
+      next();
+    });
+
+    route.all('*', harp.mount(__dirname));
+    route.all('*', function (req, res, next) {
+      req.url = '/404';
+      harp.mount(__dirname)(req, res);
+    });
+
+    console.log('Running harp-static on ' + port);
+    http.createServer(route).listen(port);
+    server(__dirname + '/public');
+  }
+}
+
+if (process.argv[2] === 'compile') {
+  harp.compile(__dirname, outputPath, function(errors){
+    if(errors) {
+      console.log(JSON.stringify(errors, null, 2));
+      process.exit(1);
+    }
+
+    process.exit(0);
+  });
+} else {
+  run();
+}
