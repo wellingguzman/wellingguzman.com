f05a4cf61fc0fb209dd146cfe1f9a537c5d37ee0
Welling Guzman <hola@wellingguzman.com>
2016-09-04 01:00:11 -0400

    only one style until it can be pick randomly

--- public/js/script.js
+++ public/js/script.js
@@ -2,50 +2,6 @@
 
   'use strict';
 
-  function getWords() {
-    return [
-      'Desarrollador Web',
-      'Web Developer',
-      'Web-Entwickler',
-      'Does nothing',
-      'Eats Taco',
-      'Pokemon Trainer',
-      'JavaScript Developer',
-      'PHP Developer',
-      'Use Objective-C once',
-      'Did printf and cout once',
-      'Backbone.js Developer',
-      'Pillow Tester',
-      'Beer Drinker',
-      'Green Socks',
-      'CSS Styler',
-    ];
-  }
-
-  function setDescription(quantity) {
-    quantity = quantity || 3;
-    var words = getWords();
-    var message = [];
-
-    if (!words) {
-      return;
-    }
-
-    for (var i = 0; i < quantity; i++) {
-      var randomIndex = getRandomInt(0, words.length, false);
-      message.push(words[randomIndex]);
-      words.splice(randomIndex, 1);
-      if (words.length == 0) {
-        break;
-      }
-    }
-
-    var description = document.getElementById('description');
-    if (description) {
-      description.innerHTML = message.join(' | ');
-    }
-  }
-
   function setDisguise(name) {
     var THIRTY_DAYS = 2592000;
     window.sessionStorage.setItem('disguise_expiration', (new Date().getTime() + THIRTY_DAYS));
@@ -54,39 +10,33 @@
 
   function addJay() {
     var head = document.getElementsByTagName('head')[0];
-    var baseStyle = document.getElementById('baseStyle');
-    var extendStyle = document.getElementById('extendStyle');
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = '/js/g.min.js?v=' + SITE_VERSION;
+    script.src = '/js/g-min.js?v=' + SITE_VERSION;
     head.appendChild(script);
 
-    var link = document.createElement('link');
-    link.type = 'text/css';
-    link.rel = 'stylesheet';
-    link.href = '/css/g.css?v=' + SITE_VERSION;
-    head.appendChild(link);
+    switchStyle('g');
+  }
 
-    head.removeChild(extendStyle);
-    head.removeChild(baseStyle);
-
-    setDisguise('jay');
+  function switchStyle(name) {
+    var style = document.getElementById('baseStyle');
+    style.href = '/css/'+name+'.css?v=' + SITE_VERSION;
   }
 
   function addBef() {
-    var style = document.getElementById('extendStyle');
-    style.href = '/css/clean.css?v=' + SITE_VERSION;
+    switchStyle('clean');
   }
 
   function addQueen() {
-    var style = document.getElementById('extendStyle');
-    style.href = '/css/queen.css?v=' + SITE_VERSION;
+    switchStyle('queen');
+  }
+
+  function addLand() {
+    switchStyle('land');
   }
 
   function addOne() {
-    var head = document.getElementsByTagName('head')[0];
-    var style = document.getElementById('extendStyle');
-    head.removeChild(style);
+    switchStyle('style');
   }
 
   function getRandomInt(min, max, maxIncluded) {
@@ -97,10 +47,9 @@
     return Math.floor(Math.random() * (max - min + includeMax)) + min;
   }
 
-  setDescription(4);
   var disguiseExpiration = window.sessionStorage.getItem('disguise_expiration');
   var disguiseName = window.sessionStorage.getItem('disguise_name');
-  var disguises = ['jay', 'one', 'bef', 'queen'];
+  var disguises = ['jay', 'one', 'land', 'bef', 'queen'];
 
   if (parseInt(disguiseExpiration, 10) < (new Date()).getTime() || disguises.indexOf(disguiseName) < 0) {
     window.sessionStorage.removeItem('disguise_expiration');
@@ -110,6 +59,8 @@
     if (randomNumber === 34) {
       disguiseName = 'jay';
     } else if (randomNumber === 1) {
+      disguiseName = 'land';
+    } else if (randomNumber === 0) {
       disguiseName = 'one';
     } else if (randomNumber === 29) {
       disguiseName = 'bef';
@@ -120,20 +71,23 @@
     setDisguise(disguiseName);
   }
 
-  switch(disguiseName) {
-    case 'jay':
-      addJay();
-      break;
-    case 'bef':
-      addBef();
-      break;
-    case 'one':
-      addOne();
-      break;
-    case 'queen':
-      addQueen();
-      break;
-  }
+  // switch(disguiseName) {
+  //   case 'jay':
+  //     addJay();
+  //     break;
+  //   case 'bef':
+  //     addBef();
+  //     break;
+  //   case 'land':
+  //     addLand();
+  //     break;
+  //   case 'one':
+  //     addOne();
+  //     break;
+  //   case 'queen':
+  //     addQueen();
+  //     break;
+  // }
 
 function anchorClickHandler(event) {
   event = event || window.event;
