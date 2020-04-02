6aae8e6bf82ffef8ac1dac4d0832a936cb9a1aec
Welling Guzman <hola@wellingguzman.com>
2020-04-02 19:30:45 +0200

    exclude less from compilation

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ var less = require('gulp-less');
 var cssmin = require('gulp-cssmin');
 
 gulp.task('less', function () {
-  return gulp.src('./public/less/**/[^_]*.less')
+  return gulp.src('./less/**/[^_]*.less')
     .pipe(less())
     .pipe(cssmin())
     .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./public/less/**/*.less', gulp.series('less'));
+  gulp.watch('./less/**/*.less', gulp.series('less'));
 });
 
 gulp.task('default', gulp.series('watch'));
